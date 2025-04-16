57e3e45f4412ba1f922767aaab5da5ebf857821e
hardwareontrial <hadwareontrial@gmail.com>
2025-04-16 13:41:28 +0700

    create server instance

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const http = require('http');
 const app = require('./app');
 const PORT = process.env.APP_PORT || 3000;
-
+const server = http.createServer(app)
 server.listen(PORT, () => {
   console.log(`Server initialized, running on 0.0.0.0:${PORT}`);
 });
