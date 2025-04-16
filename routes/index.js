38619323ba8a6aac67907f0f576068ef2c42661d
hardwareontrial <hadwareontrial@gmail.com>
2025-04-16 14:24:26 +0700

    add new routes

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,11 @@ router.get('/', (req, res) => {
   res.status(200).json({message: `API running on port ${process.env.APP_PORT}`});
 });
 
+router.get('/data', (req, res) => {
+  res.status(200).json({message: `return from /data`});
+});
+router.put('/data', (req, res) => {
+  res.status(200).json({data: req.body});
+});
+
 module.exports = router
